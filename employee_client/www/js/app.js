4f388cad12a265aa2bc5c6abdf86a85134a68e29
kasimsuzen <kasmszen@gmail.com>
2014-08-21 20:45:17 +0300

    öneri listesi ve sepet için slide efekti konuldu, öneri listesi otomatik dolduruluyor

--- employee_client/www/js/app.js
+++ employee_client/www/js/app.js
@@ -1,4 +1,4 @@
-var fikrimuhalStaj = angular.module('fikrimuhalStaj', ['ngRoute']);
+var fikrimuhalStaj = angular.module('fikrimuhalStaj', ['ngRoute','ngTouch']);
 
 	// create the controller and inject Angular's $scope
 
@@ -42,8 +42,42 @@ fikrimuhalStaj.config(function($routeProvider) {
 
 	fikrimuhalStaj.controller('customerDetailController', function($scope) {
 		$scope.message = 'Ürün seçin';
+		var list = [
+			{ "id":5, "description":"Ürün", "fiyat":601 },
+			{ "id":15, "description":"Ürün", "fiyat":602 },
+			{ "id":25, "description":"Ürün", "fiyat":603 },
+			{ "id":35, "description":"Ürün", "fiyat":604 },
+			{ "id":45, "description":"Ürün", "fiyat":605 },
+			{ "id":55, "description":"Ürün", "fiyat":606 }
+		];
+
+		var listL = [];
+		var listR = [];
+
+		for (var i = 0; i <= list.length - 1 ; i++) {
+			 
+			if (i % 2 == 0){
+				listL.push(list[i]);
+			}else{
+			 listR.push(list[i]);
+			}
+		};
+
+		$scope.productList = {left:listL, right:listR};
+		console.log(list.length);
 	});
 
 	fikrimuhalStaj.controller('cartController', function($scope) {
 		$scope.message = 'Ürün listesi';
 	});
+
+	fikrimuhalStaj.controller('SlideController', function($scope) {
+  
+	  $scope.currentSlide = 1;
+	  
+	  $scope.slideChanged = function(currSlide) {
+	    $scope.currentSlide = currSlide;
+	    console.log('Active Slide=' + $scope.currentSlide);
+	  }
+	});
+
