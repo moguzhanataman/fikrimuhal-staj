1a7a11d7bb4d7e8b83f452c7cdf1ed2058281078
kasimsuzen <kasmszen@gmail.com>
2014-08-25 16:57:42 +0300

    trying to fetch json from server

--- employee_client/www/js/app.js
+++ employee_client/www/js/app.js
@@ -72,79 +72,87 @@ fikrimuhalStaj.run(function($ionicPlatform) {
 
 });
 
-	fikrimuhalStaj.controller('mainController', function($scope) {
+fikrimuhalStaj.controller('mainController', function($scope) {
 
-		// create a message to display in our view
-		$scope.message = 'Şifreniniz girin!';
-	});
+	// create a message to display in our view
+	$scope.message = 'Şifreniniz girin!';
+});
 
 
-	fikrimuhalStaj.controller('customerListController', function($scope) {
-		$scope.message = 'Müşteri seçin.';
-	});
+fikrimuhalStaj.controller('customerListController', function($scope) {
+	$scope.message = 'Müşteri seçin.';
+});
 
-	fikrimuhalStaj.controller('customerDetailController', function($scope) {
-		$scope.message = 'Ürün seçin';
-		var list = [
-			{ "id":5, "description":"Ürün", "fiyat":601 },
-			{ "id":15, "description":"Ürün", "fiyat":602 },
-			{ "id":25, "description":"Ürün", "fiyat":603 },
-			{ "id":35, "description":"Ürün", "fiyat":604 },
-			{ "id":45, "description":"Ürün", "fiyat":605 },
-			{ "id":55, "description":"Ürün", "fiyat":606 }
-		];
 
-		var listL = [];
-		var listR = [];
+fikrimuhalStaj.controller('customerDetailController', function($scope) {
+	$scope.message = 'Ürün seçin';
+	var list = [
+		{ "id":5, "description":"Ürün", "fiyat":601 },
+		{ "id":15, "description":"Ürün", "fiyat":602 },
+		{ "id":25, "description":"Ürün", "fiyat":603 },
+		{ "id":35, "description":"Ürün", "fiyat":604 },
+		{ "id":45, "description":"Ürün", "fiyat":605 },
+		{ "id":55, "description":"Ürün", "fiyat":606 }
+	];
 
-		for (var i = 0; i <= list.length - 1 ; i++) {
-			 
-			if (i % 2 == 0){
-				listL.push(list[i]);
-			}else{
-			 listR.push(list[i]);
+	var listL = [];
+	var listR = [];
+
+	for (var i = 0; i <= list.length - 1 ; i++) {
+		 
+		if (i % 2 == 0){
+			listL.push(list[i]);
+		}else{
+		 listR.push(list[i]);
+		}
+	};
+
+	$scope.sliderState = 1;
+	$scope.productList = {left:listL, right:listR};
+
+	/* TODO  lodash kur lodashle sil */
+	function slideHasChanged(product,index,listNo){
+		console.log(" id " , product.id , " index " , index);
+
+		if(index == 2){
+
+			var indexofProductL = listL.indexOf(product);
+			console.log("indexofProductL", indexofProductL);
+			
+			if (indexofProductL > -1 ) {
+				listL.splice(indexofProductL,1);
 			}
-		};
 
-		$scope.sliderState = 1;
-		$scope.productList = {left:listL, right:listR};
-
-		/* TODO  lodash kur lodashle sil */
-		function slideHasChanged(product,index,listNo){
-			console.log(" id " , product.id , " index " , index);
-
-			if(index == 2){
-
-				var indexofProductL = listL.indexOf(product);
-				console.log("indexofProductL", indexofProductL);
-				
-				if (indexofProductL > -1 ) {
-					listL.splice(indexofProductL,1);
-				}
-
-				var indexofProductR = listR.indexOf(product);
-				console.log("indexofProductR", indexofProductR);
-				
-				if (indexofProductR > -1) {
-					listR.splice(indexofProductR,1);
-				}
+			var indexofProductR = listR.indexOf(product);
+			console.log("indexofProductR", indexofProductR);
+			
+			if (indexofProductR > -1) {
+				listR.splice(indexofProductR,1);
 			}
 		}
-		$scope.slideHasChanged = slideHasChanged;
-	});
+	}
+	$scope.slideHasChanged = slideHasChanged;
+});
 
-	fikrimuhalStaj.controller('cartController', function($scope) {
-		$scope.message = 'Ürün listesi';
-		$scope.sliderState = 1;
-	});
+fikrimuhalStaj.controller('cartController', function($scope) {
+	$scope.message = 'Ürün listesi';
+	$scope.sliderState = 1;
+});
 
-	fikrimuhalStaj.controller('SlideController', function($scope) {
+fikrimuhalStaj.controller('SlideController', function($scope) {
+
+  $scope.currentSlide = 1;
   
-	  $scope.currentSlide = 1;
-	  
-	  $scope.slideChanged = function(currSlide) {
-	    $scope.currentSlide = currSlide;
-	    console.log('Active Slide=' + $scope.currentSlide);
-	  }
-	});
+  $scope.slideChanged = function(currSlide) {
+    $scope.currentSlide = currSlide;
+    console.log('Active Slide=' + $scope.currentSlide);
+  }
+});
 
+fikrimuhalStaj.controller('PostsCtrlAjax', function($scope, $http) {
+	$http({method: 'GET', url: 'http://192.168.1.11:9000/api/customer '}).success(function(data)
+	{
+		$scope.costumerList = data; // response data
+		console.log("afasdf");
+	});
+})
