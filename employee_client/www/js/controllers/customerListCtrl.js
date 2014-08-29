e7a4b2661a8488d694179ab6a2d65227f0460e3b
kasimsuzen <kasmszen@gmail.com>
2014-08-29 12:04:12 +0300

    Parçalanması gereken arrayler splitArrayle parçalandı

--- employee_client/www/js/controllers/customerListCtrl.js
+++ employee_client/www/js/controllers/customerListCtrl.js
@@ -5,21 +5,7 @@ fikrimuhalStaj.controller('customerListController', ['$scope', 'loginService', f
 
 fikrimuhalStaj.controller('PostsCtrlAjax', function ($scope, $http) {
     $http({method: 'GET', url: config.api.urls.customerList }).success(function (data) {
-        var customerlistL = [];
-        var customerlistR = [];
 
-        /* TODO bir listeyi ikiye ayıracak fonksiyon yazılacak */
-
-        for (var i = 0; i <= data.customers.length - 1; i++) {
-
-            if (i % 2 == 0) {
-                customerlistL.push(data.customers[i]);
-            } else {
-                customerlistR.push(data.customers[i]);
-            }
-        }
-
-
-        $scope.customerList = {left: customerlistL, right: customerlistR};
+        $scope.customerList = splitArray(data.customers);
     });
 });
