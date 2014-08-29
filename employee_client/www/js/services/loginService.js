ff5eec5dafb911e6ba2073839f96b9a9a9e2fee8
Ilgaz Şumnulu <ilgaz@fikrimuhal.com>
2014-08-29 15:21:06 +0300

    login ekranı kisi adları

--- employee_client/www/js/services/loginService.js
+++ employee_client/www/js/services/loginService.js
@@ -4,12 +4,12 @@ fikrimuhalStaj.factory('loginService', ['$http', '$q' , function loginService($h
     console.log("loginService  initialized");
 
     var mockEmployees = [
-        {'id': 2, 'photoUrl': "/img/placeholder_large.png", 'passwordHash': 2468},
-        {'id': 3, 'photoUrl': "/img/placeholder_large.png", 'passwordHash': 2468},
-        {'id': 4, 'photoUrl': "/img/placeholder_large.png", 'passwordHash': 2468},
-        {'id': 1, 'photoUrl': "/img/placeholder_large.png", 'passwordHash': 2468},
-        {'id': 5, 'photoUrl': "/img/placeholder_large.png", 'passwordHash': 2468},
-        {'id': 6, 'photoUrl': "/img/placeholder_large.png", 'passwordHash': 2468}
+        {'id': 1, 'photoUrl': "./img/avatar-big.png", name:"Kasım Süzen", 'passwordHash': 2468},
+        {'id': 2, 'photoUrl': "./img/avatar-big.png", name:"Oğuz Ataman", 'passwordHash': 2468},
+        {'id': 3, 'photoUrl': "./img/avatar-big.png", name:"Ilgaz Şumnulu", 'passwordHash': 2468},
+        {'id': 4, 'photoUrl': "./img/avatar-big.png", name:"Şükrü Hasdemir", 'passwordHash': 2468},
+        {'id': 5, 'photoUrl': "./img/avatar-big.png", name:"Orhan Gencebay", 'passwordHash': 2468},
+        {'id': 6, 'photoUrl': "./img/avatar-big.png", name:"Zeki Müren", 'passwordHash': 2468}
     ];
 
     var _loggedinEmployee = null;
