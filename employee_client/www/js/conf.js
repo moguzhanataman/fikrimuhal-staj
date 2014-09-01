7197e54603f1db55c4118a64d793d992913fa250
M. Oğuzhan Ataman <thekhug@gmail.com>
2014-09-01 13:27:39 +0300

    cartCtrl, cartService tamamlandı.
    customerDetailCtrl'daki list değişkeninin başında var yoktu onu da ekledim

--- employee_client/www/js/conf.js
+++ employee_client/www/js/conf.js
@@ -6,7 +6,9 @@ config.api.ip = "192.168.1.11";
 config.api.port = "9000";
 config.api.base ="http://" + config.api.ip + ":" +config.api.port + "/";
 config.api.urls = { 
-	customerList: config.api.base + "api/customers/current" };
+	customerList: config.api.base + "api/customers/current",
+	employeeList: config.api.base + "api/employees"
+};
 
 var constant = {};
 constant.cartSlider = {"ADD_ITEM": 0, "REMOVE_ITEM": 2, "CURRENT_ITEM": 1};
