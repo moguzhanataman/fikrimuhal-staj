316306bdc8fe254bfd0140fb96bae9ac4e3f485a
kasimsuzen <kasmszen@gmail.com>
2014-09-02 20:11:21 +0300

    Clientta dinamik olarak uuid, identifier, minor ve major değişkenleri değiştirilebiliyor

--- customer_client/www/js/controllers/customerInfoCtrl.js
+++ customer_client/www/js/controllers/customerInfoCtrl.js
@@ -1,3 +1,14 @@
 fikrimuhalStajCustomer.controller('customerInfoCtrl',[ '$scope', '$state', 'customerService', 'loginService' , function($scope,$state,customerService,loginService) {
+	function parser(){
+
+		console.log("value is", $scope.uuidValue, $scope.majorValue, $scope.minorValue, $scope.idValue);
+	}
+
+	$scope.uuidValue = 'DA5336AE-2042-453A-A57F-F80DD34DFCD9';
+	$scope.majorValue = 5;
+	$scope.minorValue = 1000;
+	$scope.idValue = 'beaconAtTheMacBooks';
+
+	$scope.sender=parser;
 
 }]);
