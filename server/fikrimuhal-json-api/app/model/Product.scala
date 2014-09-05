768737472e73a2c9759313ef7960b02c45dc7a8b
Ilgaz Şumnulu <ilgaz@fikrimuhal.com>
2014-09-05 22:08:25 +0300

    yeni demo ürünleri, bagzilarinin ismi uzun

--- server/fikrimuhal-json-api/app/model/Product.scala
+++ server/fikrimuhal-json-api/app/model/Product.scala
@@ -12,6 +12,17 @@ object Product {
   implicit val jsonConverter = Json.format[Product]
 
   val all = Seq(
+    Product(11, "NORTH COAST", 135.99, 1, "./img/products/jeans/11.jpg"),
+    Product(12, "M&S COLLECTION", 335.99, 1, "./img/products/jeans/12.jpeg"),
+    Product(13, "Water Resistant Jeans with Comfort Stretch", 354, 1, "./img/products/jeans/13.jpeg"),
+    Product(14, "Basic Wash Jeans", 12, 1, "./img/products/jeans/14.jpeg"),
+    Product(15, "Big & Tall Denim Jeans with Comfort Stretch", 321, 1, "./img/products/jeans/11.jpg"),
+    Product(16, "Tapered Leg Denim Jeans with ", 34, 1, "./img/products/jeans/15.jpeg"),
+    Product(17, "Smart Jeans with StayNEW", 222, 1, "./img/products/jeans/16.jpeg"),
+    Product(18, "Tapered Jeans with Comfort Stretch", 443, 1, "./img/products/jeans/17.jpeg"),
+    Product(19, "Jeans with Comfort Stretch", 999, 1, "./img/products/jeans/18.jpeg"),
+    Product(20, "Rinse Washed Stretch Denim Jeans", 735.99, 1, "./img/products/jeans/19.jpeg"),
+    Product(21, "Water Resistant Jeans with Comfort Stretch", 22.99, 1, "./img/products/jeans/20.jpeg"),
     Product(1, "Kırmızı tişört", 35.99, 1, "./img/products/t-shirt.jpg"),
     Product(2, "Kot pantolon", 95.00, 2, "./img/products/kot.jpg"),
     Product(3, "Gömlek", 10.50, 3, "./img/products/shirt.jpg"),
