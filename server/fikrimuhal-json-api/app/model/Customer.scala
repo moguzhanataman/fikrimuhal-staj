c175dd504b45dd676cc140f3580d14b8e1862c01
M. Oğuzhan Ataman <thekhug@gmail.com>
2014-09-09 16:28:12 +0300

    Bazı commentlenmiş mock datalar silindi.

--- server/fikrimuhal-json-api/app/model/Customer.scala
+++ server/fikrimuhal-json-api/app/model/Customer.scala
@@ -39,17 +39,6 @@ object Customer {
     Customer(16, "Emirhan Mavi Sarıyar",  profilePicPath(6), 1409061949, 1),
     Customer(17, "Suat Mavi Ağgez",       profilePicPath(7), 1409061949, 1),
     Customer(18, "Furkan Mavi Tektaş",    profilePicPath(8), 1409061949, 1)
-    //    Customer(9, "Mehmet", photoData, 1409061949, 1),
-    //    Customer(10, "Mehmet", photoData, 1409061949, 1),
-    //    Customer(11, "Mehmet", photoData, 1409061949, 1),
-    //    Customer(12, "Mehmet", photoData, 1409061949, 1),
-    //    Customer(13, "Mehmet", photoData, 1409061949, 1),
-    //    Customer(14, "Mehmet", photoData, 1409061949, 1),
-    //    Customer(15, "Mehmet", photoData, 1409061949, 1),
-    //    Customer(16, "Mehmet", photoData, 1409061949, 1),
-    //    Customer(17, "Mehmet", photoData, 1409061949, 1),
-    //    Customer(18, "Mehmet", photoData, 1409061949, 1),
-    //    Customer(19, "Hilmi", photoData, 1409061949, 1)
   )
 
   def get(id: Int): Option[Customer] = all.find(id == _.id)
